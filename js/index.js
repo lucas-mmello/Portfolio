bcbac73bb0fd83467864e3511b4f9aabfd22ea6f
lucas mello <lucasmello03@hotmail.com>
2023-03-14 15:20:41 -0300

    alterado o form

--- js/index.js
+++ js/index.js
@@ -30,9 +30,8 @@ window.onscroll = () => {
 
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
-
-  ScrollReveal({ reset: true, distance: "80px", duration: 2000, delay: 200 });
 };
+ScrollReveal({ reset: true, distance: "80px", duration: 2000, delay: 200 });
 
 ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
 ScrollReveal().reveal(
